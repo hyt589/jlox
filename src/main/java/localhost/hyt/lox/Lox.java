75259f112f870aec18a43b06d48c97ed92a93669
hyt <yxh589@case.edu>
2020-12-13 17:29:52 +0800

    implemented a recursive descent parser

--- src/main/java/localhost/hyt/lox/Lox.java
+++ src/main/java/localhost/hyt/lox/Lox.java
@@ -55,14 +55,28 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens)
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError)
         {
-            System.out.println(token);
+            return;
         }
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
         report(line, "", message);
+    }
+
+    static void error(Token token, String message)
+    {
+        if (token.type == TokenType.EOF) {
+            report(token.line, "at end", message);
+        } else {
+            report(token.line, "at '" + token.lexeme + "'", message);
+        }
     }
 
     private static void report(int line, String where, String message) {
